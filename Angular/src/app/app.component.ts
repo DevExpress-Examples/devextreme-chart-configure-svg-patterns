eb64d6263406c838e9d853c79d70d2ce9be06c7d
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-08-12 16:54:32 +0300

    Update Angular

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,20 +1,50 @@
 import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
+import { Item, Service } from './app.service';
+import { PointInfo, PointConfiguration, ChartAttributes } from './chart.types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [Service],
 })
 export class AppComponent {
-  title = 'Angular';
+  dataSource: Item[] | null;
 
-  counter = 0;
+  chartAttributes: ChartAttributes;
 
-  buttonText = 'Click count: 0';
+  constructor(service: Service) {
+    this.dataSource = service.getDataSource();
+    this.chartAttributes = {
+      class: 'chart-class',
+    };
+  }
 
-  onClick(e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  customizePoint(info: PointInfo): PointConfiguration {
+    const style: PointConfiguration = {};
+
+    switch (info.argument) {
+      case 'Monday':
+        style.color = 'url(#Gradient1)';
+        break;
+      case 'Tuesday':
+        style.color = 'url(#Gradient2)';
+        break;
+      case 'Wednesday':
+        style.color = 'url(#Gradient3)';
+        break;
+      case 'Thursday':
+        style.color = 'url(#Pattern1)';
+        break;
+      case 'Friday':
+        style.color = 'url(#PointImage)';
+        break;
+      case 'Saturday':
+        style.color = 'url(#TriangleSvg)';
+        break;
+      default:
+        break;
+    }
+    return style;
   }
 }
